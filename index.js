8cabd6bcd4553c59a485e725d3fdff7b0ff6b555
Richpolis Systems <richpolis@gmail.com>
2016-11-20 10:03:45 -0600

    agregando el modelo y creando la peticion POST

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const Product = require('./models/product');
+
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -20,8 +22,21 @@ app.get('/api/products/:productId',(req, res) => {
 });
 
 app.post('/api/products',(req, res) => {
+	console.log('POST /api/products');
 	console.log(req.body);
-	res.status(200).send({message: 'producto se ha recibido'});
+
+	let product = new Product();
+	product.name = req.body.name;
+	product.picture = req.body.picture;
+	product.price = req.body.price;
+	product.category = req.body.category;
+	product.description = req.body.description;
+	
+	product.save((err, productStored) => {
+		if(err) res.status(500).send({message: `Error en guardar en la base de datos ${err}`});
+
+		res.status(200).send({product: productStored});
+ 	});
 });
 
 app.put('/api/products/:productId',(req, res) => {
